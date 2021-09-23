e5870dae199e74478a82540d9ae664ef75bc80a9
ManuelInfante <infantemanuel.mail@gmail.com>
2021-09-23 23:13:24 +0200

    milestone 1

--- main.js
+++ main.js
@@ -3,9 +3,23 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#app',
     data:{
-        
+       newToDo: '', 
+       toDos: [
+        'Fare i compiti',
+        'Fare la spesa',
+        'Fare il bucato',
+       ],
     },
     methods: {
-       
+        addToDo() {
+            if(!this.newToDo == ''){
+                this.toDos.push(this.newToDo);
+            };
+            this.newToDo = '';
+        },
+
+        removeToDo(index) {
+            this.toDos.splice(index, 1);
+        }
     },
 });
