8311011954e3d3ddec8364464c6a22ec8d4eba9e
ManuelInfante <infantemanuel.mail@gmail.com>
2021-09-24 01:28:09 +0200

    funzione doneToggle e grafica- fine progetto

--- main.js
+++ main.js
@@ -5,21 +5,40 @@ const app = new Vue({
     data:{
        newToDo: '', 
        toDos: [
-        'Fare i compiti',
-        'Fare la spesa',
-        'Fare il bucato',
+        {
+            title: 'Fare i compiti',
+            done: false,
+        },
+        {
+            title: 'Fare la spesa',
+            done: false,
+        },
+        {
+            title: 'Fare il bucato',
+            done: false,
+        },
        ],
+
+       classiDone: ['notDone', 'done']
     },
     methods: {
         addToDo() {
             if(!this.newToDo == ''){
-                this.toDos.push(this.newToDo);
+                this.toDos.push({
+                   title: this.newToDo,
+                   done: false,
+                });
             };
             this.newToDo = '';
         },
 
         removeToDo(index) {
             this.toDos.splice(index, 1);
+        },
+
+        doneToggle(index) {
+            this.toDos[index].done = !this.toDos[index].done
+            // console.log(this.toDos[index].done)
         }
     },
 });
